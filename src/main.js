ced5b4c4d8230e5419582f11f279083d63a649d2
Kristy12936 <sdin0045@student.monash.edu>
2025-07-08 11:42:08 +0800

    feat: complete responsive Form.vue with Bootstrap 5 and card display

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import './assets/main.css'
+// import './style.css'  //  注释掉自定义样式（Activity 3 用的）
+
+import 'bootstrap/dist/css/bootstrap.min.css'  //  新增：引入 Bootstrap 样式
 
 import { createApp } from 'vue'
 import App from './App.vue'
